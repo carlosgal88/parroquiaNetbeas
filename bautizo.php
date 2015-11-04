b083be2d3fcf7d07aa241e48dc1f643838540efe
carlosgal88 <carlosjosegaliciaazenon@gmail.com>
2015-11-03 22:30:18 -0600

    s
    ss

---
+++ bautizo.php
@@ -0,0 +1,37 @@
+<!-- y finaliza por 12 de Octubre de 2015 index Estructura basica 
+Carlos Jose Galicia-->
+
+<!DOCTYPE html
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Parroquia Atiquizaya Bautizos</title>
+	<link rel="stylesheet" type="text/css" href="estilosMenu.css">
+	<link rel="stylesheet" type="text/css" href="founts.css">
+
+</head>
+<body>
+
+		<header>
+			<nav>
+				<ul>
+                                    <li><a href="index.php"><span class=""><i class="icon icon-home2"></i></span>Inicio</a></li>
+                                    <li><a href="index.php"><span class=""><i class="icon icon-library_books"></i></span>Agenda</a></li>
+                                    <li><a href="bautizo.php"><span class=""><i class="icon icon-contacts"></i></span>Bautizo</a>
+						<ul>
+                                                    <li><a href="supletoriaBauitizos.php">Supletorias de Fe de Bautizo</a></li>
+						</ul>
+
+					</li>
+                                        <li><a href="confirmacion.php"><span class=""><i class="icon icon-contacts"></i></span>Confirmacion</a></li>
+                                        <li><a href="Matrimonio.php"><span class=""><i class="icon icon-users"></i></span>Matrimonio</a></li>
+
+				</ul>	
+
+			</nav>		
+
+		</header>	
+
+</body>
+</html><!--me base en este Tuto 
+https://www.youtube.com/watch?v=z-CPRslZS_A-->
